d933c9a0b9ed332276ee9456554c8fc789709bf7
unknown <gdutt2304@gmail.com>
2026-03-30 15:23:50 +0530

    express

---
+++ express/server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello Avneesh");
+});
+
+server.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${3000}`);
+});
